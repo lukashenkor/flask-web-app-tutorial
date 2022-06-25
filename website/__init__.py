2fa9a57e4fa90978321b9827b98934dc199e1abc
Roman Lukashenko <ramosss1997@gmail.com>
2022-06-25 14:51:27 +0200

    add basic routes

--- website/__init__.py
+++ website/__init__.py
@@ -5,4 +5,10 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'jkdjaskeqwjklji38921u8uj#*(Yhu13'
 
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     return app
