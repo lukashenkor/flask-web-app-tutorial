f166e501efb10b194aad8061e1eba6f114a21ba0
Roman Lukashenko <ramosss1997@gmail.com>
2022-06-25 16:09:28 +0200

    add signup flashed messages. add db creation

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+
+db = SQLAlchemy()
+DB_NAME = 'database.db'
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'jkdjaskeqwjklji38921u8uj#*(Yhu13'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -11,4 +19,15 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+
+    create_database(app)
+
+
     return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
